876744ee2416c3865d496d97e340af4464b0f1d8
foteiza <fede.oteiza@gmail.com>
2024-09-12 21:34:30 -0300

    Terminando el 15 y arreglando el 10

--- Practico-3/src/js/main.js
+++ Practico-3/src/js/main.js
@@ -132,10 +132,10 @@ function ej10() {
   let num1 = Number(document.querySelector("#txtEj10").value);
   let num2 = Number(document.querySelector("#txtEj10b").value);
   let msj9 = "";
-  let resultado2 = num1;
+  let resultado2 = 1;
 
   for (let i = num1; i <= num2; i++) {
-    resultado2 *= num2;
+    resultado2 *= i;
   }
 
   msj9 += `El resultado es: ${resultado2}`;
@@ -152,5 +152,94 @@ function ej11() {
   let num2 = Number(document.querySelector("#txtEj11b").value);
   let num3 = Number(document.querySelector("#txtEj11c").value);
   let msj10 = "";
-  let resultado3 = 0;
+
+  if (num1 < num2) {
+    for (let i = num1; i <= num2; i++) {
+      if (i % num3 === 0) {
+        msj10 += i + ", ";
+      }
+    }
+  } else if (num2 < num1) {
+    for (let i = num2; i <= num1; i++) {
+      if (i % num3 === 0) {
+        msj10 += i + ", ";
+      }
+    }
+  }
+
+  document.querySelector(
+    "#pEj11"
+  ).innerHTML = `Los numeros multiplos son: ${msj10}`;
+}
+
+//Ejercicio 12
+document.querySelector("#btnEj12").addEventListener("click", ej12);
+
+function ej12() {
+  let num1 = Number(document.querySelector("#txtEj12").value);
+  let msj11 = "";
+  let resultado3 = 1;
+
+  for (let i = 1; i <= num1; i++) {
+    resultado3 *= i;
+  }
+
+  msj11 += `El resultado es: ${resultado3}`;
+
+  document.querySelector("#pEj12").innerHTML = msj11;
+}
+
+//Ejercicio 13
+
+document.querySelector("#btnEj13").addEventListener("click", ej13);
+
+function ej13() {
+  let alto = Number(document.querySelector("#txtEj13").value);
+  let ancho = Number(document.querySelector("#txtEj13b").value);
+  let msj12 = "";
+
+  for (let i = 0; i < alto; i++) {
+    for (let j = 0; j < ancho; j++) {
+      msj12 += "#";
+    }
+    msj12 += "<br>";
+  }
+
+  document.querySelector("#pEj13").innerHTML = msj12;
+}
+
+//Ejercicio 14
+document.querySelector("#btnEj14").addEventListener("click", ej14);
+
+function ej14() {
+  let numD = Number(document.querySelector("#txtEj14").value);
+  let msj13 = "";
+  let cantidadNum = 0;
+
+  while (numD >= 1) {
+    numD = numD / 10;
+    cantidadNum++;
+  }
+
+  msj13 += `La cantidad de digitos es: ${cantidadNum}`;
+  document.querySelector("#pEj14").innerHTML = msj13;
+}
+
+//Ejercicio 15
+document.querySelector("#btnEj15").addEventListener("click", ej15);
+
+function ej15() {
+  let num1 = Number(document.querySelector("#txtEj15").value);
+  let msj14 = "";
+
+  if (num1 >= 2000) {
+    while (num1 >= 2000 && num1 > 100) {
+      num1 = num1 / 20;
+      msj14 += `${num1}`;
+    }
+  } else {
+    msj14 += "Ingrese un numero correcto, mayor a 2000";
+  }
+
+  document.querySelector("#pEj15").innerHTML = msj14;
 }
