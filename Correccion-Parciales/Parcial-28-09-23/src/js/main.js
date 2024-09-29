fad78ad3d8a8d9b604b6b0f0a954e5dee7e7e25f
foteiza <fede.oteiza@gmail.com>
2024-09-29 19:20:15 -0300

    Pronto parcial 28-09-23

--- Correccion-Parciales/Parcial-28-09-23/src/js/main.js
+++ Correccion-Parciales/Parcial-28-09-23/src/js/main.js
@@ -1,14 +1,13 @@
 document.querySelector("#btnEnviar").addEventListener("click", calcular);
 
+let gananciasTotal = 0;
+
 function calcular() {
   let nombreBanda = document.querySelector("#txtBanda").value;
   let horasEnsayo = Number(document.querySelector("#txtHoras").value);
   let sala = document.querySelector("#slcSalas").value;
   let valorHora = 380;
   let valorTotal = valorHora * horasEnsayo;
-  let gananciasTotal = 0;
-  let mensaje1 = "";
-  let mensaje2 = "";
 
   if (nombreBanda === "") {
     mensaje1 += "Datos invalidos";
@@ -16,22 +15,49 @@ function calcular() {
     mensaje1 += "Datos invalidos";
   }
 
-  if (nombreBanda.toLowerCase().indexOf("panda") !== -1) {
+  if (nombreBanda.indexOf("o") !== nombreBanda.lastIndexOf("o")) {
+    valorTotal = (valorTotal * 45) / 100;
+  } else if (nombreBanda.toLowerCase().includes("panda")) {
     valorTotal = (valorTotal * 35) / 100;
-    mensaje1 += "Valor a pagar: " + valorTotal;
   } else if (horasEnsayo > 3) {
     valorTotal = (valorTotal * 20) / 100;
-    mensaje1 += "Valor a pagar: " + valorTotal;
-  } else if (nombreBanda.indexOf("o") !== nombreBanda.lastIndexOf("o")) {
-    valorTotal = (valorTotal * 45) / 100;
-    mensaje1 += "Valor a pagar: " + valorTotal;
-  } else if (sala === "pardo") {
+  }
+
+  if (sala === "pardo") {
     valorTotal -= 200;
   }
 
   gananciasTotal += valorTotal;
-  mensaje2 += "Ganancias totales: " + gananciasTotal;
 
-  document.querySelector("#pMensaje1").innerHTML = mensaje1;
-  document.querySelector("#pMensaje2").innerHTML = mensaje2;
+  document.querySelector("#pMensaje1").innerHTML = `Valor total: ${valorTotal}`;
+  document.querySelector(
+    "#pMensaje2"
+  ).innerHTML = `Ganancias totales: ${gananciasTotal}`;
+}
+
+//Ejercicio 2
+document.querySelector("#btnBoton").addEventListener("click", mostrarTexto);
+
+function mostrarTexto() {
+  let texto = document.querySelector("#txtTexto").value;
+  let caracteresTexto = texto.length;
+  let primerCaracter = texto.charAt(0);
+  let contadorLetras = 0;
+
+  texto = texto.toLowerCase();
+
+  for (let i = 0; i < texto.length; i++) {
+    let codigo = texto.charAt(i);
+
+    if (codigo === primerCaracter) {
+      contadorLetras++;
+    }
+  }
+
+  document.querySelector("#pResultado").innerHTML =
+    "Cantidad de caracteres: " +
+    caracteresTexto +
+    "<br>" +
+    "Cantidad de letras iguales al primer caracter: " +
+    contadorLetras;
 }
